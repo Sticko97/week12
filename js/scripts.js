0ab3555f734cc066c1a7461129df2ddd32edb953
Sticko97 <sle23@my.bcit.ca>
2022-11-22 14:55:02 -0800

    remove eventlistener, and {once:true}

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 const clickButton = document.querySelector('.click-me')
 // console.log(clickButton);
-function clickHandler(clickButton)
+function clickHandler()
 {
     alert("You clicked me!")
+    // clickButton.removeEventListener("click", clickHandler);
 }
-clickButton.addEventListener("click", clickHandler);
+clickButton.addEventListener("click", clickHandler, {once:true} );
+
+
+
